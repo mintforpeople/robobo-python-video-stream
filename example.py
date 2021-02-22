f7d26b69da6ded570856fbadae0f68d8c70625de
Shiul93 <lfllamas93@gmail.com>
2021-02-22 17:37:31 +0100

    Modified example

--- example.py
+++ example.py
@@ -3,19 +3,30 @@ import cv2
 
 videoStream = RoboboVideo("192.168.31.241")
 
-
 def main():
     print("Starting test app")
 
     videoStream.connect()
 
     print("Showing images")
-
+    #rob.setLaneColorInversion(False)
+    i = 0
+    last_ts = 0
     while True:
-        cv2_image = videoStream.getImage()
-        cv2.namedWindow('imagen', cv2.WINDOW_NORMAL)
-        cv2.imshow('imagen', cv2_image)
-        cv2.waitKey(1)
+        i+=1
+        frame, timestamp, sync_id, frame_id = videoStream.getImageWithMetadata()
+
+        if timestamp != last_ts:
+            print(timestamp)
+            print(sync_id)
+            print(frame_id)
+            cv2.imshow('smartphone camera', frame)
+
+        last_ts = timestamp
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            videoStream.disconnect()
+            cv2.destroyAllwindows()
+            break
 
 
 main()
